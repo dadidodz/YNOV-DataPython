03f0165fefcf9e1fff7e593b5de5e8de38e8ab29
dadidodz <dorian.blatiere@hotmail.fr>
2024-04-01 12:03:28 +0200

    changes

--- Partie1/Q17.py
+++ Partie1/Q17.py
@@ -1,6 +1,6 @@
 #Création d'un tuple contenant les mois de l'année
-tupleMois = ("Janvier", "Fevrier", "Mars", "Avril", "Mai", 
+tuple_mois = ("Janvier", "Fevrier", "Mars", "Avril", "Mai", 
              "Juin", "Juillet", "Aout", "Septembre", 
              "Octobre","Novembre","Decembre")
 
-print(tupleMois)
+print(tuple_mois)
