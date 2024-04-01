99c360d49ecc0ab1e2ac1d0a384a82fb8d9338a6
dadidodz <dorian.blatiere@hotmail.fr>
2024-04-01 12:10:31 +0200

    changes

--- Partie2/Q30.py
+++ Partie2/Q30.py
@@ -1,6 +1,6 @@
 #Dictionnaire de livres ayant comme comme clé un nom de livre
 #et comme valeur leur auteur
-dictLivres = {
+livres = {
     "Le Petit Chaperon rouge" : "Charles Perrault",
     "1984": "George Orwell",
     "Le Petit Prince": "Antoine de Saint-Exupery",
@@ -8,5 +8,5 @@ dictLivres = {
 }
 
 # Affichage du titre et de l'auteur de chaque livre
-for titre, auteur in dictLivres.items():
+for titre, auteur in livres.items():
     print(f"Titre: {titre} - Auteur: {auteur}")
