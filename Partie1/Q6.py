a19ed4c3654de3842c9db6304f8a419018652660
dadidodz <dorian.blatiere@hotmail.fr>
2024-04-01 11:54:41 +0200

    changes

--- Partie1/Q6.py
+++ Partie1/Q6.py
@@ -1,8 +1,8 @@
 #Création de deux variables stockants le résultat d'une division
-varA = 8 / 4
-print(type(varA))
-print(varA)
+var_a = 8 / 4
+print(type(var_a))
+print(var_a)
 
-varB = 8 // 4
-print(type(varB))
-print(varB)
+var_b = 8 // 4
+print(type(var_b))
+print(var_b)
