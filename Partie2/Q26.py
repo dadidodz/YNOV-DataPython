8b884cd91bf36d3054e9d3e475ebffa461b2908a
dadidodz <dorian.blatiere@hotmail.fr>
2024-04-01 12:06:02 +0200

    changes

--- Partie2/Q26.py
+++ Partie2/Q26.py
@@ -1,9 +1,9 @@
 #Liste de nombres
-listeNombre = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+liste_nombres = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-print(listeNombre)
+print(liste_nombres)
 
 #Remplacement des nombres par leurs carrés dans la même liste
-listeNombre = [x**2 for x in listeNombre]
+liste_nombres = [x**2 for x in liste_nombres]
 
-print(listeNombre)
+print(liste_nombres)
