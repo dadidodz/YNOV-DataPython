131014a79818db3742339828e16ce32fad711de9
dadidodz <dorian.blatiere@hotmail.fr>
2024-04-01 11:53:44 +0200

    changes

--- Partie1/Q4.py
+++ Partie1/Q4.py
@@ -1,7 +1,7 @@
-#Création d'une variable unString, contenant une chaîne de caractères "1234"
-unString = "1234"
-print(type(unString))
+#Création d'une variable var, contenant une chaîne de caractères "1234"
+var = "1234"
+print(type(var))
 
-#Conversion de la variable unString de type string à un type int
-unString = int(unString)
-print(type(unString))
+#Conversion de la variable var de type string à un type int
+var = int(var)
+print(type(var))
