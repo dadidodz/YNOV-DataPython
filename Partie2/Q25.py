10ca6428d346959cf5b3ce4ba6d456adbd0d086b
dadidodz <dorian.blatiere@hotmail.fr>
2024-04-01 12:04:41 +0200

    changes

--- Partie2/Q25.py
+++ Partie2/Q25.py
@@ -1,7 +1,7 @@
 #Liste de nombre
-listeNombres = [12, 45, 87, 30, 1, 5, 16, 48, 98]
+liste_nombres = [12, 45, 87, 30, 1, 5, 16, 48, 98]
 
 #For affichant les nombres paires
-for nombre in listeNombres:
+for nombre in liste_nombres:
     if nombre % 2 ==0:
         print(nombre)
