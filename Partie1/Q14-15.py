41987dab97be4ed057c8c9f9a23c717e4ce1e460
dadidodz <dorian.blatiere@hotmail.fr>
2024-04-01 12:01:43 +0200

    changes

--- Partie1/Q14-15.py
+++ Partie1/Q14-15.py
@@ -4,17 +4,15 @@ phrase = "Bonjour, je suis un etudiant en informatique"
 #Découpage de la phrase mot par mot et stockant dans une liste
 liste_mots = phrase.split()
 
-print(liste_mots)
+#Inversion de l'ordre des mots de la phrase
+liste_mots.reverse()
 
-# #Inversion de l'ordre des mots de la phrase
-# liste_mots.reverse()
+#Création vide d'une varible qui stockera notre phrase inversée
+phrase_inverse = ""
 
-# #Création vide d'une varible qui stockera notre phrase inversée
-# phrase_inverse = ""
+#Parcours de notre liste de mot inversé et concaténation dans la 
+#variable "phrase_inverse"
+for mot in liste_mots:
+    phrase_inverse += mot + " "
 
-# #Parcours de notre liste de mot inversé et concaténation dans la 
-# #variable "phrase_inverse"
-# for mot in liste_mots:
-#     phrase_inverse += mot + " "
-
-# print(phrase_inverse)
+print(phrase_inverse)
